782c1c2c3b2676aa3441c57813c89a490f2ed25c
Abner <abner.math.c@gmail.com>
2016-06-30 22:23:31 -0300

    Added yata mirror

--- game/dota_addons/ninpou2/scripts/vscripts/items/akatsuki_set.lua
+++ game/dota_addons/ninpou2/scripts/vscripts/items/akatsuki_set.lua
@@ -13,25 +13,33 @@ function ShadowAmbushThink(event)
 	local inaction_duration = ability:GetSpecialValueFor("inaction_duration")
 	-- If idle, passively apply the fade out
 	ability:CreateVisibilityNode(caster:GetAbsOrigin(), 1000, 5.0)
+	if not caster.shadow_ambush then 
+		caster.shadow_ambush = 0
+	end
+	if not caster.shadow_ambush_duration then 
+		caster.shadow_ambush_duration = 0
+	end
 	if caster:IsIdle() and not caster:GetAttackTarget() then
 		if not caster:HasModifier("modifier_shadow_ambush") then
-			if not caster.shadow_ambush then 
-				caster.shadow_ambush = 0
-			end
 			caster.shadow_ambush = caster.shadow_ambush + 0.5
 			if caster.shadow_ambush >= fade_time + inaction_duration then 
 				ability:ApplyDataDrivenModifier(caster, caster, "modifier_shadow_ambush", {})
 				ability:ApplyDataDrivenModifier(caster, caster, "modifier_invisible", {})
 			end
 		else
-			if not caster.shadow_ambush_duration then 
-				caster.shadow_ambush_duration = 0
-			elseif caster.shadow_ambush_duration < 10 then
+			if caster.shadow_ambush_duration < 10 then
 				caster.shadow_ambush_duration = caster.shadow_ambush_duration + 0.5
 			end
 		end
 	else
 		if caster.shadow_ambush_duration > 0 then 
+			local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_slark/slark_dark_pact_pulses.vpcf", PATTACH_ABSORIGIN, caster)
+			ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControlEnt(particle, 1, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControl(particle, 2, Vector(300, 0, 0))
+			ParticleManager:SetParticleControlEnt(particle, 3, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControlEnt(particle, 4, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControlEnt(particle, 5, caster, PATTACH_ABSORIGIN, "attach_origin", caster:GetAbsOrigin(), true)
 			caster.shadow_ambush_bonus = caster.shadow_ambush_duration
 			ability:ApplyDataDrivenModifier(caster, caster, "modifier_shadow_ambush_buff", {duration=6})
 		end
--- game/dota_addons/ninpou2/scripts/vscripts/modifiers/modifier_shadow_ambush_buff.lua
+++ game/dota_addons/ninpou2/scripts/vscripts/modifiers/modifier_shadow_ambush_buff.lua
@@ -12,7 +12,7 @@ function modifier_shadow_ambush_buff:OnCreated()
 	if caster.shadow_ambush_bonus then
 		self.move_bonus = caster.shadow_ambush_bonus * 10.0
 		self.attack_bonus = caster.shadow_ambush_bonus * caster:GetAverageTrueAttackDamage() / 10.0
-		self.particle = ParticleManager:CreateParticle("particles/econ/items/templar_assassin/templar_assassin_focal/templar_assassin_meld_focal.vpcf", PATTACH_ABSORIGIN, caster)
+		self.particle = ParticleManager:CreateParticle("", PATTACH_ABSORIGIN, caster)
 		ParticleManager:SetParticleControlEnt(self.particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true) 
 		ParticleManager:SetParticleControlEnt(self.particle, 1, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
 	end
